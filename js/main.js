1f3bce2f46a39c82114dfba3bf6d0feeaba57812
Hamidjonova-N <shamsuddinovanigoraxon@gmail.com>
2023-04-12 11:52:07 +0500

    Add click events

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 "use strict";
 
+let elLightModeBtn = document.querySelector(".light-mode-btn");
+let elDarkModeBtn = document.querySelector(".dark-mode-btn");
+
+elLightModeBtn.addEventListener("click", function () {
+  document.body.classList.remove("dark-mode");
+});
+
+elDarkModeBtn.addEventListener("click", function () {
+  document.body.classList.add("dark-mode");
+});
